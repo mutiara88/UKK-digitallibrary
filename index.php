dca4acee4fc9f113b2842b4ed166b0a11193d322
mutiara88 <160831398+mutiara88@users.noreply.github.com>
2024-02-27 12:43:23 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,83 +1,97 @@
+<?php 
+	session_start();
+ 
+	// cek apakah yang mengakses halaman ini sudah login
+	if($_SESSION['Level']==""){
+		header("location:index.php?pesan=info_login");
+	}
+ 
+	?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title> Form Login APP Digital</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Petugas</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
   integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        <div class="content">
-            <div class="card mt-5 mb-5">
-                <div class="row">
-                    <div class="col-sm-6 text-center">
-                        <img src="assets/img/login.png" width="500">
-                    </div>
-                    <div class="col-sm-6">
-
-                        <div class="card-body">
-                            <h5>Masukan Akun Anda</h5>
-                            
-                            <?php
-                            if(isset($_GET['pesan'])){
-                                if($_GET['pesan']=="gagal"){
-
-                            echo "<div class= 'alert alert-danger'>UserName dan Password tidak sesuai.!!</div>";
-                                }
-                            }
-                            if(isset($_GET['pesan'])){
-                            if($_GET['pesan']=="info_login"){
-
-                                echo "<div class= 'alert alert-info'>Maaf Anda Belum Login.!!</div>";
-                                }
-                            }
-                            if(isset($_GET['pesan'])){
-                            if($_GET['pesan']=="info_logout"){
-
-                                echo "<div class= 'alert alert-success'>Anda Berhasil Logout</div>";
-                                }
-                            }
-                            if(isset($_GET['pesan'])){
-                                if($_GET['pesan']=="info_daftar"){
-    
-                                    echo "<div class= 'alert alert-success'>Anda Berhasil Daftar</div>";
-                                    }
-                                }
-                            
-                            ?>
-
-                            <form method="post" action="cek_login.php">
-                                <div class="form-group mt-3">
-                                    <label>UserName</label>
-                                    <input type="text" name="Username" class="form-control">
-                                </div>
-                           
-                                <div class="form-group mt-2">
-                                    <label>Password </label>
-                                    <input type="Password" name="Password" class="form-control">
-                                </div>
-
-                                <div class="form-group mt-2">
-                                    <button type="submit" class="btn btn-primary">Login</button>
-                                </div>
-                                <div class="form-group mt-2">
-                                    <label>Belum Punya Akun? :</label>
-                                    <a href="daftar.php" class="btn btn-warning btn-sm">Daftar Akun</a>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-               </div>
-            </div>
-        </div>
-    </div>
-             
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist
+	<div class ="container text-center">
+	<div class="content mt-3">
+		<div class="card bg-primary bg-gradient">
+			<div class="card-body">
+				
+				<a href="#" class ="btn text-light">HOME</a>
+				<a href="kategoribuku.php" class ="btn text-light">KATEGORI BUKU</a>
+				<a href="http://localhost/digitallibrary/admin/tampil_buku.php" class ="btn text-light">BUKU</a>
+				<a href="http://localhost/digitallibrary/admin/user/tampil_user.php" class ="btn text-light" >USERS</a>
+				<a href="http://localhost/digitallibrary/peminjam/form_peminjaman.php/" class ="btn text-light">PEMINJAMAN</a>
+				<a href="http://localhost/digitallibrary/peminjam/laporanpeminjam.php" class ="btn text-light">LAPORAN PEMINJAMAN</a>
+				<a href="../logout.php" class ="btn text-light">LOGOUT</a>
+			</div>
+		</div>
+	</div>
+		<div class="content mt-3">
+				<div class="row">
+					<div class="col-sm-3">
+					<div class="card">
+						<div class="card-body bg-info text-center" style="--bs-bg-opacity: .5;">
+							<h3>INPUT DATA BUKU</h3>
+							<h2>^-^</h2>
+							<hr>
+						<a href = "buku.php" class ="btn btn-dark  d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">Lihat Data</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+					<div class="card">
+						<div class="card-body bg-info text-center" style="--bs-bg-opacity: .5;">
+							<h3> KATEGORI BUKU</h3>
+							<h2>^0^</h2>
+							<hr>
+						<a href = "kategoribuku.php" class ="btn btn-dark  d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">Lihat Data</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+					<div class="card">
+					<div class="card-body bg-info text-center" style="--bs-bg-opacity: .5;">
+							<h3>USER</h3>
+							<h2>^-^</h2>
+							<hr>
+						<a href = "../admin/user/tampil_user.php" class ="btn btn-dark d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">Lihat Data</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+					<div class="card">
+						<div class="card-body bg-info text-center" style="--bs-bg-opacity: .5;">
+							<h3>PEMINJAMAN</h3>
+							<h2>^0^ </h2>
+							<hr>
+						<a href = "http://localhost/digitallibrary/peminjam/form_peminjaman.php/" class="btn btn-dark d-inline-flex focus-ring py-1 px-2 text-decoration-none border rounded-2">Lihat Data</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="content mt-3"> 
+			<div class="card">
+				<div class="card-body">
+					<p>Halo<b><?php echo $_SESSION['Username']; ?></b> Anda Telah Login Sebagai<b><?php 
+					echo $_SESSION['Level']; ?></b>.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="content mt-3"> 
+			<p class="text-center">@2024 UKK Aplikasi Perpustakaan Digital By.Mutiara Cantika</p>
+	</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist
     /umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/
     bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
-</html>
 
-
- 
+</html>	 
--- koneksi.php
+++ koneksi.php
@@ -4,4 +4,3 @@ if (mysqli_connect_errno()){
     echo "Koneksi database gagal : " . mysqli_connect_error();
 }
 ?>
-
